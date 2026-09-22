d3bb76887a4590755f4fb7fee0324e8f24c10015
Ricky <riccardorighi39@gmail.com>
2026-09-22 11:42:57 +0200

    es 1 primo

---
+++ Primo_Progetto/www/public/hello-world.php
@@ -0,0 +1,17 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>hello-world</title>
+</head>
+    <body>
+        <h1>
+            <?php
+            echo "hello ".$_GET['nome']."!";
+            ?>
+        </h1>
+        <p>questo è il primo programma</p>
+    </body>
+</html>
